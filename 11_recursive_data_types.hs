b5ef640d9f5a89ac9a628fff4f0dd460e096de83
Tejas Bubane <tejasbubane@gmail.com>
2018-05-12 23:59:21 +0530

    Recursive data types

---
+++ 11_recursive_data_types.hs
@@ -0,0 +1,24 @@
+-- Binary search trees
+
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Eq)
+
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node a left right)
+  | x == a = Node x left right
+  | x < a = Node a (treeInsert x left) right
+  | x > a = Node a left (treeInsert x right)
+
+-- check if element is in the tree
+treeElem :: (Ord a) => a -> Tree a -> Bool
+treeElem x EmptyTree = False
+treeElem x (Node a left right)
+  | x == a = True
+  | x < a = treeElem x left
+  | x > a = treeElem x right
+
+nums = [8,5,3,6,9,1,3]
+numsTree = foldr treeInsert EmptyTree nums
