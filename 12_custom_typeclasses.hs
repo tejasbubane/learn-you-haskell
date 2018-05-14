b202204a8a853bcaa94a753e41ec1170784679d0
Tejas Bubane <tejasbubane@gmail.com>
2018-05-14 08:13:03 +0530

    Define custom typeclasses and instantiate(implement) them

---
+++ 12_custom_typeclasses.hs
@@ -0,0 +1,33 @@
+data TrafficLight = Red | Yellow | Green
+
+-- Implementing `Show` by hand (without deriving)
+instance Show TrafficLight where
+  show Red = "Red Light"
+  show Yellow = "Yellow Light"
+  show Green = "Green Light"
+
+class YesNo a where
+  yesno :: a -> Bool
+
+instance YesNo Int where
+  yesno 0 = False
+  yesno _ = True
+
+instance YesNo [a] where
+  yesno [] = False
+  yesno _ = True
+
+instance YesNo Bool where
+  yesno = id
+
+instance YesNo (Maybe a) where
+  yesno (Just _) = True
+  yesno Nothing = False
+
+instance YesNo TrafficLight where
+  yesno Red = False
+  yesno _ = True
+
+yesnoIf :: (YesNo y) => y -> a -> a -> a
+yesnoIf yesnoVal yesResult noResult =
+  if yesno yesnoVal then yesResult else noResult
